b3207bd4876857dca5da6a443fc0505ecada9e09
KDGehlot2003 <kdgehlot2003@gmail.com>
2023-12-30 22:08:46 +0530

    new

--- test.py
+++ test.py
@@ -8,14 +8,4 @@ if __name__ == '__main__':
     max_val=max(lst)
     lst.remove(max_val)
     print(max(lst))
-
-    # print(max_val)
-    # lst = []
-    # for i in arr:
-    #     lst.append(i)
-    # print(lst)
-    # print(type(lst))
-    # print(arr)
-    # print(type(arr))
-    # # print(max(arr))
-    # print(arr)
+    
