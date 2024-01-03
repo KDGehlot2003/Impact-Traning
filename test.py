a77809f1ffb9c7dce8e3f8918f8484604846ce33
KDGehlot2003 <kdgehlot2003@gmail.com>
2024-01-03 23:39:57 +0530

    new

--- test.py
+++ test.py
@@ -1,9 +1,22 @@
-n = int(input())
-l = []
-count = 0
-for i in range(0,n):
-    l.append(input())
-for i in l:
-    if int(i[-4:-2]) >= 60:
-        count += 1
-print(count)
+if __name__ == '__main__':
+    arr = []
+    for i in range(int(input())):
+        name = input()
+        score = float(input())
+        arr.append([name,score])
+    # print(arr)
+    min_val = min(arr)
+    # print(min_val)
+    arr.remove(min_val)
+    # print(arr)
+    min_val = min(arr)
+    arr2 = []
+    for i in arr:
+        # print(i[1])
+        # print(min_val[1])
+        if i[1]== min_val[1]:
+            arr2.append(i[0])
+    arr2.sort()
+    print(arr2)
+    for i in arr2:
+        print(i)
