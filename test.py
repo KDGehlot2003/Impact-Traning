4355baec519c36115331c8f1d85b402c145dcb01
KDGehlot2003 <kdgehlot2003@gmail.com>
2024-03-09 13:37:50 +0530

    9th of March 2024

--- test.py
+++ test.py
@@ -1,48 +1,10 @@
-# Input:
-# UDDLLRUUUDUURUDDUULLDRRRR
-# 5
-# 5
-# 2
-# 2 2
-# 3 3
+# x = 1534236469
 
+# l = list(range(-2**31,2**31-1))
 
-s = input()
-row = int(input())
-col = int(input())
+# if x in l:
+#     print("Yes")
+# else:
+#     print(False)
 
-o = int(input())
-
-ob1 = list(map(int, input().split()))
-ob2 = list(map(int, input().split()))
-
-ele = 0
-l=[]
-for _ in range(row):
-    
-    a = []
-    for _ in range(col):
-        ele += 1
-        a.append(ele)
-    l.append(a)
-    
-
-for i in l:
-    print(i)
-
-i = j = 0
-
-for mom in s:
-    if i!=ob1[0] or j!=ob1[1] and i!=ob2[0] or j!=ob2[1]:
-        if mom=="U":
-            i -= 1
-        elif mom=="D":
-            i += 1
-        elif mom=="L":
-            j -= 1
-        elif mom=="R":
-            j += 1
-    
-
-
-print("Final position of the robot: ({}, {})".format(abs(j),abs(i)))
+print(2**31-1534236469)
